840a82160d2fc62c188dcccc35f134be45f7b782
azhimeblog <azhime.blog@gmail.com>
2021-02-21 17:14:01 +0700

    add

--- routes/getgamefc.js
+++ routes/getgamefc.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
 
-    fs.readFile('./../public/json/gamefc.json', (err, data) => {
+    fs.readFile('/app/public/json/gamefc.json', (err, data) => {
         if (err) throw err;
         let cheat = JSON.parse(data);
         res.send(cheat);
