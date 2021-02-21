f495577e3356eb2b9d01d485cbceb198df87c637
azhimeblog <azhime.blog@gmail.com>
2021-02-21 16:44:02 +0700

    update CORS

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -7,6 +8,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var updateapiRouter = require('./routes/updateapi');
+var getgamepgapiRouter = require('./routes/getgamepg');
+var getgamesgapiRouter = require('./routes/getgamesg');
+var getgamejokerapiRouter = require('./routes/getgamejoker');
+var getgamefcapiRouter = require('./routes/getgamefc');
 
 var app = express();
 
@@ -20,10 +25,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use(cors())
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/jsonapi', updateapiRouter);
+app.use('/updateapi', updateapiRouter);
 app.use(express.static('public'))
+app.use('/getgamepg', getgamepgapiRouter);
+app.use('/getgamesg', getgamesgapiRouter);
+app.use('/getgamejoker', getgamejokerapiRouter);
+app.use('/getgamefc', getgamefcapiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/getgamesg.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+const fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
+    fs.readFile('../public/json/gamesg.json', (err, data) => {
+        if (err) throw err;
+        let cheat = JSON.parse(data);
+        res.send(cheat);
+    });
+
+});
+
+module.exports = router;
